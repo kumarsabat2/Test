af523267ae0897cfcd033abd38ee9922eabd8c98
kumarsabat2 <66631241+kumarsabat2@users.noreply.github.com>
2020-06-09 16:15:48 +0530

    Sentiment analysis with feedback
    It will return positive or negative

--- Sentiment analysis.py
+++ Sentiment analysis.py
@@ -1,12 +1,20 @@
-def RPAChallenge(message):
+def RPAChallenge(text):
     from textblob import TextBlob
-    line = message
-    blob = TextBlob(line)
-    print(blob.sentiment)
-    print()
+    from textblob.sentiments import NaiveBayesAnalyzer
+    import nltk
+    sent = TextBlob(text)
+    polarity      = sent.sentiment.polarity
+    subjectivity = sent.sentiment.subjectivity
+    sent = TextBlob(text,analyzer = NaiveBayesAnalyzer())
+    classification = sent.sentiment.classification
+    positive      = sent.sentiment.p_pos
+    negative      = sent.sentiment.p_neg
+    print("Feedback is :" + classification,positive,negative)
+    
+  
 
 
 
-m = "I love tea"
+m = "Awesome product"
 
 RPAChallenge(m)
